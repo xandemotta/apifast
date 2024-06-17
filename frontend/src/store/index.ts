7547129ad9dc184bd449e0ac55d44b2496fb7800
xandemotta <85975693+xandemotta@users.noreply.github.com>
2024-06-17 17:13:49 -0300

    tentativa numero 545456465

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,20 +1,17 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    count: 0,
+  },
+  getters: {
   },
   mutations: {
-    increment(state) {
-      state.count++;
-    },
   },
   actions: {
-    increment(context) {
-      context.commit("increment");
-    },
   },
-});
+  modules: {
+  }
+})
